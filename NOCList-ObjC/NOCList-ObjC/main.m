c5ec16965cb2b2b3cb6ac5e975a191556c1aa07d
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-16 12:28:27 -0700

    Added starter project and LSILog files

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Jorge Alvarez on 3/16/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LSILog.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
