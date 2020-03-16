320665851c430d6a9a5f8274a12de7c883cdbfb2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-16 15:47:34 -0700

    Step 6 done

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -12,7 +12,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+                
         // MARK: - Step 2
         
         LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
@@ -71,7 +71,22 @@ int main(int argc, const char * argv[]) {
         // MARK: - Step 6
         
         NSLog(@"STEP 6 ------");
-
+        int lowCount = 0;
+        int midCount = 0;
+        int highCount = 0;
+        for (LSIAgent *agent in agentsArray) {
+            if (agent.accessLevel.intValue >= 8) {
+                highCount += 1;
+            }
+            else if (agent.accessLevel.intValue >= 5 && agent.accessLevel.intValue <= 7) {
+                midCount += 1;
+            }
+            else {
+                lowCount += 1;
+            }
+        }
+        NSLog(@"%i low level agents, %i mid level agents, and %i high level agents", lowCount, midCount, highCount);
+        NSLog(@"");
     }
     return 0;
 }
