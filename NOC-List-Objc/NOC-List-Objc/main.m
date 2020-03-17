d077f47d2a5a578757ec63ab119396f513a6b373
Aaron Cleveland <toscleveland@gmail.com>
2020-03-16 20:13:49 -0500

    init

---
+++ NOC-List-Objc/NOC-List-Objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOC-List-Objc
+//
+//  Created by Aaron Cleveland on 3/16/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
