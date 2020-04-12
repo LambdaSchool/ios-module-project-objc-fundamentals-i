b38b1f51d5893a11a08c3c501b088616627b7a3f
Alex Shillingford <alex.shillingford@icloud.com>
2020-04-11 22:10:48 -0600

    Complete Step 2: Create LSIAgent object for every agent listed
    Add all agents to NSArray

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -7,11 +7,79 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        LSIAgent *eHunt = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt"
+                                                     realName:@"Tom Cruise"
+                                                  accessLevel:(int *)8
+                                                  compromised:(BOOL *)NO];
+        
+        LSIAgent *jPhelps = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps"
+                                                       realName:@"Jon Voight"
+                                                    accessLevel:(int *)9
+                                                    compromised:(BOOL *)YES];
+        
+        LSIAgent *cPhelps = [[LSIAgent alloc] initWithCoverName:@"Claire Phelps"
+                                                       realName:@"Emmanuelle Beart"
+                                                    accessLevel:(int *)5
+                                                    compromised:(BOOL *)NO];
+        
+        LSIAgent *eKittridge = [[LSIAgent alloc] initWithCoverName:@"Eugene Kittridge"
+                                                          realName:@"Henry Czerny"
+                                                       accessLevel:(int *)10
+                                                       compromised:(BOOL *)YES];
+        
+        LSIAgent *fKrieger = [[LSIAgent alloc] initWithCoverName:@"Franz Krieger"
+                                                        realName:@"Jean Reno"
+                                                     accessLevel:(int *)4
+                                                     compromised:(BOOL *)NO];
+        
+        LSIAgent *lStickell = [[LSIAgent alloc] initWithCoverName:@"Luther Stickell"
+                                                         realName:@"Ving Rhames"
+                                                      accessLevel:(int *)4
+                                                      compromised:(BOOL *)NO];
+        
+        LSIAgent *sDavies = [[LSIAgent alloc] initWithCoverName:@"Sarah Davies"
+                                                       realName:@"Kristin Scott Thomas"
+                                                    accessLevel:(int *)5
+                                                    compromised:(BOOL *)YES];
+        
+        LSIAgent *mRotGrab = [[LSIAgent alloc] initWithCoverName:@"Max RotGrab"
+                                                        realName:@"Vanessa Redgrave"
+                                                     accessLevel:(int *)4
+                                                     compromised:(BOOL *)NO];
+        
+        LSIAgent *hWilliams = [[LSIAgent alloc] initWithCoverName:@"Hannah Williams"
+                                                         realName:@"Ingeborga Dapkūnaitė"
+                                                      accessLevel:(int *)5
+                                                      compromised:(BOOL *)YES];
+        
+        LSIAgent *jHarmon = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon"
+                                                       realName:@"Emilio Esteves"
+                                                    accessLevel:(int *)6
+                                                    compromised:(BOOL *)YES];
+        
+        LSIAgent *fBarnes = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes"
+                                                       realName:@"Dale Dye"
+                                                    accessLevel:(int *)9
+                                                    compromised:(BOOL *)NO];
+        
+        NSArray *agents = [[NSArray alloc] initWithObjects:eHunt,
+                                                           jPhelps,
+                                                           cPhelps,
+                                                           eKittridge,
+                                                           fKrieger,
+                                                           lStickell,
+                                                           sDavies,
+                                                           mRotGrab,
+                                                           hWilliams,
+                                                           jHarmon,
+                                                           fBarnes, nil];
     }
     return 0;
 }
