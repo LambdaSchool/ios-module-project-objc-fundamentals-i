82d721e552b652158ae589279078d0be843b5f75
Enrique Gongora <EGTech24@gmail.com>
2020-04-13 12:43:29 -0700

    Initial Commit

---
+++ NOClist-ObjC/NOClist-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOClist-ObjC
+//
+//  Created by Enrique Gongora on 4/13/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
---
+++ NOClist-ObjC/NOClist-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOClist-ObjC
+//
+//  Created by Enrique Gongora on 4/13/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
