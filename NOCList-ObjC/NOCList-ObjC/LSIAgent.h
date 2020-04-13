d6865f0c77be4c3aba04930c96b16d4061cec311
Keri Levesque <kerianne166@gmail.com>
2020-04-13 15:39:34 -0400

    added .h file

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,17 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Keri Levesque on 4/13/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Keri Levesque on 4/13/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
