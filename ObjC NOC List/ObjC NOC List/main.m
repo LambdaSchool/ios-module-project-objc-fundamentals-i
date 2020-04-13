56eb83f40d633c5615ed0d2e624afddf7b30f24e
Chris <chris.gonzales.82@gmail.com>
2020-04-13 12:29:54 -0700

    Created NOC list command line project

---
+++ ObjC NOC List/ObjC NOC List/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC NOC List
+//
+//  Created by Chris Gonzales on 4/13/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
