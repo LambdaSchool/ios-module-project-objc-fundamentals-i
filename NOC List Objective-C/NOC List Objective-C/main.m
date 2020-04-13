f4932b6a3823b740d5c09307026ff084812108f3
elizabeth paige <wingateep@Icloud.com>
2020-04-13 15:39:44 -0400

    Step 0

---
+++ NOC List Objective-C/NOC List Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOC List Objective-C
+//
+//  Created by Elizabeth Wingate on 4/13/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
