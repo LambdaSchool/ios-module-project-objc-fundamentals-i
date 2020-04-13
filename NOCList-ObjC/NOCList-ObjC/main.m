be1eaf782c61b1685be00e4d322bac073f7a15a4
Keri Levesque <kerianne166@gmail.com>
2020-04-13 15:32:43 -0400

    added starter project

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Keri Levesque on 4/13/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
