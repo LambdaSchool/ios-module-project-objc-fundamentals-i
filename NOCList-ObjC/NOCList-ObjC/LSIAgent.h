fd3496829b7179f9162cf459812e3247f340ff66
Keri Levesque <kerianne166@gmail.com>
2020-04-13 18:11:45 -0400

    finished project

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.h
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -12,6 +12,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIAgent : NSObject
 
+@property NSString *coverName;
+@property NSString *realName;
+@property NSNumber *accessLevel;
+@property NSNumber *compromised;
+
+- (instancetype) initWithCoverName:(NSString *)coverName
+                          realName: (NSString *)realName
+                       accessLevel: (int)accessLevel compromised: (BOOL)compromised;
+
+
+
 @end
 
 NS_ASSUME_NONNULL_END
