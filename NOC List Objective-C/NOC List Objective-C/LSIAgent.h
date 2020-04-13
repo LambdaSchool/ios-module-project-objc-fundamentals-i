5c3b3ed5eb21358bf895a9fd24c5f892c7fd11cc
elizabeth paige <wingateep@Icloud.com>
2020-04-13 15:54:11 -0400

    made model class for LSIAgents

---
+++ NOC List Objective-C/NOC List Objective-C/LSIAgent.h
@@ -0,0 +1,22 @@
+//
+//  LSIAgent.h
+//  NOC List Objective-C
+//
+//  Created by Elizabeth Wingate on 4/13/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@property NSString *coverName;
+@property NSString *realName;
+@property NSNumber *accessLevel;
+@property NSNumber *compromised;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOC List Objective-C/NOC List Objective-C/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOC List Objective-C
+//
+//  Created by Elizabeth Wingate on 4/13/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
