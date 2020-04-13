94a4b758d5d5267cae4fe98a7018d3dbacd8169e
Nick <ptnguyen1901@gmail.com>
2020-04-13 17:14:43 -0400

    Step 1 completed.

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Nick Nguyen on 4/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
