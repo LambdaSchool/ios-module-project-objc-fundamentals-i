fc6a3dbab7f2ca4a7ecbadf10388ecb075b9f80c
Hector L <hectoremil21@hotmail.com>
2020-05-10 18:49:54 -0400

    Create an LSIAgent .h & .m. Implement properties, and instancetype initializer with all parameters. accessLevel is int. compromised is BOOL.

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Karen Rodriguez on 5/10/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Karen Rodriguez on 5/10/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
