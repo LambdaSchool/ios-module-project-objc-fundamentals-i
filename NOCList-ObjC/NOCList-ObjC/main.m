4f2c828947785e563cdfcf7919f588e52344750d
Hector L <hectoremil21@hotmail.com>
2020-05-10 19:08:26 -0400

    Create NSArray with all agents.

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -27,6 +27,7 @@ int main(int argc, const char * argv[]) {
         LSIAgent *agent10 = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:TRUE];
         LSIAgent *agent11 = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:FALSE];
 
+        NSArray *nocList = @[agent1, agent2, agent3, agent4, agent5, agent6, agent7, agent8, agent9, agent10, agent11];
     }
     return 0;
 }
