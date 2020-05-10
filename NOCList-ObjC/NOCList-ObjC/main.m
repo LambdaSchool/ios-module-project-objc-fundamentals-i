cb50f1cdf3f4566771fbc7fc608bb6c643eb4362
Hector L <hectoremil21@hotmail.com>
2020-05-10 18:59:15 -0400

    Ccreate first agent.

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -7,11 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+
+        LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tome Cruise" accessLevel:8 compromised:FALSE];
+
     }
     return 0;
 }
