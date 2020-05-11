cdfde83f6c857ed28b5c190ca000ecf3aa6ccb7e
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-11 16:10:36 -0400

    Part 1 complete. Create a model class called LSIAgent, created init and object properties

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,28 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Wyatt Harrell on 5/11/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+
+//coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false
+
+@property NSString *coverName;
+@property NSString *realName;
+@property NSNumber *accessLevel;
+@property NSNumber *compromised;
+
+- (instancetype)initWithCoverName:(NSString *)coverName realName:(NSString *)realName accessLevel:(int)accessLevel compromised:(BOOL)compromised;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
