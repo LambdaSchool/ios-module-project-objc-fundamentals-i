f63ef3b81e98177e196e8b8ca022cec3c501ed46
Shawn Gee <shawngee@gmail.com>
2020-05-11 16:10:27 -0400

    Added initial project

---
+++ NOCList/NOCList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList
+//
+//  Created by Shawn Gee on 5/11/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
