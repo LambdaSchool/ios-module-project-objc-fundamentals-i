a907d0a57289fca6690eb01242b7861cd146085b
Mark Gerrior <github@gerrior.com>
2020-05-11 21:29:18 -0700

    Added LSIAgent object

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Mark Gerrior on 5/11/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
