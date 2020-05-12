b3cef8236e3ff32ce17d12dbe12d5acb9c35cecc
Shawn Gee <shawngee@gmail.com>
2020-05-12 10:59:39 -0400

    Updated descriptions

--- NOCList/NOCList/LSIAgent.h
+++ NOCList/NOCList/LSIAgent.h
@@ -29,24 +29,24 @@ typedef struct AgentCounts {
                         isCompromised:(BOOL)isCompromised;
 
 /// Counts the total number of compromised agents, which is returned as an int.
-/// @param agents The agents array to tally.
+/// @param agents The array of LSIAgents to tally.
 +(int)numberOfCompromisedAgentsInArray:(NSArray *)agents;
 
 /// Counts the total number of clean agents, which is returned as an int,
 /// as well as printing out each clean agent found.
-/// @param agents The agents array to tally.
+/// @param agents The array of LSIAgents to tally.
 +(int)numberOfCleanAgentsInArray:(NSArray *)agents;
 
 /// Prints all high risk agents, appending a warning for those that are compromised.
-/// @param agents The agents array to print.
+/// @param agents The array of LSIAgents to print.
 +(void)printHighRiskAgentsInArray:(NSArray *)agents;
 
 /// Counts the total of low, mid, and high risk agents, returning them in an AgentCounts struct.
-/// @param agents The agents array to tally.
+/// @param agents The array of LSIAgents to tally.
 +(AgentCounts)countLowMidHighRiskAgentsInArray:(NSArray *)agents;
 
 /// Prints agent cover names and access levels sorted by access level ascending, and then cover name ascending.
-/// @param agents The agents array to print.
+/// @param agents The array of LSIAgents to print.
 +(void)printSortedAgentsWithArray:(NSArray *)agents;
 
 @end
