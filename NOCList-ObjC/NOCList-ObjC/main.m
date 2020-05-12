be518f291810b818af23ca8b8528e4dd295571fe
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-12 01:50:45 +0100

    Fixed Issue with Array and Created A For Loop Checking for Compromised Agents

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -12,21 +12,29 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-        LSIAgent * ethan = [[LSIAgent alloc] initWithCoverName: @"Ethan Hunt" realName: @"Tom Cruise" accessLevel: 8 compromised: NO];
-        LSIAgent * jim = [[LSIAgent alloc] initWithCoverName: @"Jim Phelps" realName: @"Jon Voight" accessLevel: 9 compromised: YES];
-        LSIAgent * claire = [[LSIAgent alloc] initWithCoverName: @"Claire Phelps"  realName: @"Emmanuelle Beart" accessLevel: 5 compromised: NO];
-        LSIAgent * eugene = [[LSIAgent alloc] initWithCoverName: @"Eugene Kittridge" realName: @"Henry Czerny" accessLevel: 10 compromised: YES];
-        LSIAgent * franz = [[LSIAgent alloc] initWithCoverName: @"Franz Krieger"  realName: @"Jean Reno" accessLevel: 4 compromised: NO];
-        LSIAgent * luther = [[LSIAgent alloc] initWithCoverName: @"Luther Stickell"  realName: @"Ving Rhames" accessLevel: 4 compromised: NO];
-        LSIAgent * sarah = [[LSIAgent alloc] initWithCoverName: @"Sarah Davies"  realName: @"Kristin Scott Thomas" accessLevel: 5 compromised: YES];
-        LSIAgent * max = [[LSIAgent alloc] initWithCoverName: @"Max RotGrab"  realName: @"Vanessa Redgrave" accessLevel: 4 compromised: NO];
-        LSIAgent * hannah = [[LSIAgent alloc] initWithCoverName: @"Hannah Williams" realName: @"Ingeborga Dapkūnaitė" accessLevel: 5 compromised: YES];
-        LSIAgent * jack = [[LSIAgent alloc] initWithCoverName: @"Jack Harmon" realName: @"Emilio Estevez" accessLevel: 6 compromised: YES];
-        LSIAgent * frank = [[LSIAgent alloc] initWithCoverName: @"Frank Barnes" realName: @"Dale Dye" accessLevel: 9 compromised: NO];
+        LSIAgent * ethan = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
+        LSIAgent * jim = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
+        LSIAgent * claire = [[LSIAgent alloc] initWithCoverName:@"Claire Phelps"  realName:@"Emmanuelle Beart" accessLevel:5 compromised:NO];
+        LSIAgent * eugene = [[LSIAgent alloc] initWithCoverName:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel:10 compromised: YES];
+        LSIAgent * franz = [[LSIAgent alloc] initWithCoverName:@"Franz Krieger"  realName:@"Jean Reno" accessLevel:4 compromised:NO];
+        LSIAgent * luther = [[LSIAgent alloc] initWithCoverName:@"Luther Stickell"  realName:@"Ving Rhames" accessLevel:4 compromised:NO];
+        LSIAgent * sarah = [[LSIAgent alloc] initWithCoverName:@"Sarah Davies"  realName:@"Kristin Scott Thomas" accessLevel:5 compromised:YES];
+        LSIAgent * max = [[LSIAgent alloc] initWithCoverName:@"Max RotGrab"  realName:@"Vanessa Redgrave" accessLevel:4 compromised:NO];
+        LSIAgent * hannah = [[LSIAgent alloc] initWithCoverName:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel:5 compromised:YES];
+        LSIAgent * jack = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:YES];
+        LSIAgent * frank = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:NO];
 
-        NSArray * agents = @[ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank];
+        NSMutableArray * agents = [[NSMutableArray alloc] initWithObjects:ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank , nil];
 
+        int compromisedAgents = 0;
 
+        for (LSIAgent * agent in agents) {
+            if (agent.compromised.boolValue == YES) {
+                compromisedAgents++;
+            }
+        }
+
+        NSLog(@"Number of compromised agents: %d", compromisedAgents);
     }
     return 0;
 }
