9f079a3358517113e70a4c6952bcbaaf5c7e9960
Mark Gerrior <github@gerrior.com>
2020-05-11 18:18:30 -0700

    Added NOCList-ObjC

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Mark Gerrior on 5/11/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
