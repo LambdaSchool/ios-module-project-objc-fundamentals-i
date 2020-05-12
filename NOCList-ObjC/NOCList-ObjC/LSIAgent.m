5f02633e1ffdcb043036388a994bdfa96b2aa41e
Mark Gerrior <github@gerrior.com>
2020-05-11 21:42:13 -0700

    Step 1 complete. Class properites created and init method created.

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -10,4 +10,13 @@
 
 @implementation LSIAgent
 
+- (instancetype)initWithCoverName:(NSString *)coverName realName:(NSString *)realName accessLevel:(int)accessLevel compromised:(BOOL)compromised {
+    self = [super init];
+    _coverName = coverName;
+    _realName = realName;
+    _accessLevel = [NSNumber numberWithInt:accessLevel];
+    _compromised = [NSNumber numberWithBool:compromised];
+    return self;
+}
+
 @end
