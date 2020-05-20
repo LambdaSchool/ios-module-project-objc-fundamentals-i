0a89093f1b3f4dc1bbf6912f43ee2f6513d04d31
David Wright <davidtwright89@gmail.com>
2020-05-20 01:51:56 -0400

    Created a macOS command line Xcode project named "NOCList-ObjC"

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by David Wright on 5/20/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
