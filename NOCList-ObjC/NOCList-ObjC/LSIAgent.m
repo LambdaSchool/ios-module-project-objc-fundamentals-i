121572b98c50bfe3d7ba01e4d842d5fd9568b29d
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-20 13:57:27 -0400

    Implemented the custom init method

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -10,4 +10,23 @@
 
 @implementation LSIAgent
 
+// Step 3: Implement the custom init method in the implementation file of the class.
+// Be sure to convert the int and BOOL arguments passed in to NSNumber objects.
+- (instancetype)initWithCoverName: (NSString *)coverName
+                         realName: (NSString *)realName accessLevel: (int)accessLevel compromised: (BOOL)compromised {
+    self = [super init];
+
+    if(self != nil) {
+        NSNumber * accessLevelNumber = [NSNumber numberWithInt: accessLevel];
+        NSNumber * compromisedNumber = [NSNumber numberWithBool: compromised];
+
+        _coverName = coverName;
+        _realName = realName;
+        _accessLevel = accessLevelNumber;
+        _compromised = compromisedNumber;
+    }
+
+    return self;
+}
+
 @end
