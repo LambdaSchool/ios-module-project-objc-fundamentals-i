418c1653bb5dd5076417740c384d783edfbf6c73
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-20 13:46:36 -0400

    Created LSIAgent model

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,17 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Joshua Rutkowski on 5/20/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Joshua Rutkowski on 5/20/20.
+//  Copyright © 2020 Rutkowski. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
