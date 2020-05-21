21d0bc15b08aaffd99ca7ad34701c6984cfad4f6
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-20 20:13:53 -0600

    Create Initial Project inside repo

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Alex Shillingford on 5/20/20.
+//  Copyright © 2020 shillwil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
