9e7f5dc384e1050dcd87519ffccbc0d140a3cddf
Jessie <jessie.griffin@icloud.com>
2020-05-20 22:35:47 -0400

    Adds NOCList-ObjC project

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,17 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Jessie Ann Griffin on 5/20/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Jessie Ann Griffin on 5/20/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
