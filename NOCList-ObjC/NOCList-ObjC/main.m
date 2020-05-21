19ec4e135dd73d1c0f244fe78d9d96cf9b9a985c
David Wright <davidtwright89@gmail.com>
2020-05-20 22:17:54 -0400

    Removed unnecessary parentheses when initializing agent1... agent11

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -14,17 +14,17 @@ int main(int argc, const char * argv[]) {
         
         // MARK: - Step 2
         
-        LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@("Ethan Hunt") realName:@("Tom Cruise") accessLevel:8 compromised:NO];
-        LSIAgent *agent2 = [[LSIAgent alloc] initWithCoverName:@("Jim Phelps") realName:@("Jon Voight") accessLevel:9 compromised:YES];
-        LSIAgent *agent3 = [[LSIAgent alloc] initWithCoverName:@("Claire Phelps") realName:@("Emmanuelle Beart") accessLevel:5 compromised:NO];
-        LSIAgent *agent4 = [[LSIAgent alloc] initWithCoverName:@("Eugene Kittridge") realName:@("Henry Czerny") accessLevel:10 compromised:YES];
-        LSIAgent *agent5 = [[LSIAgent alloc] initWithCoverName:@("Franz Krieger") realName:@("Jean Reno") accessLevel:4 compromised:NO];
-        LSIAgent *agent6 = [[LSIAgent alloc] initWithCoverName:@("Luther Stickell") realName:@("Ving Rhames") accessLevel:4 compromised:NO];
-        LSIAgent *agent7 = [[LSIAgent alloc] initWithCoverName:@("Sarah Davies") realName:@("Kristin Scott Thomas") accessLevel:5 compromised:YES];
-        LSIAgent *agent8 = [[LSIAgent alloc] initWithCoverName:@("Max RotGrab") realName:@("Vanessa Redgrave") accessLevel:4 compromised:NO];
-        LSIAgent *agent9 = [[LSIAgent alloc] initWithCoverName:@("Hannah Williams") realName:@("Ingeborga Dapkūnaitė") accessLevel:5 compromised:YES];
-        LSIAgent *agent10 = [[LSIAgent alloc] initWithCoverName:@("Jack Harmon") realName:@("Emilio Estevez") accessLevel:6 compromised:YES];
-        LSIAgent *agent11 = [[LSIAgent alloc] initWithCoverName:@("Frank Barnes") realName:@("Dale Dye") accessLevel:9 compromised:NO];
+        LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
+        LSIAgent *agent2 = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
+        LSIAgent *agent3 = [[LSIAgent alloc] initWithCoverName:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel:5 compromised:NO];
+        LSIAgent *agent4 = [[LSIAgent alloc] initWithCoverName:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel:10 compromised:YES];
+        LSIAgent *agent5 = [[LSIAgent alloc] initWithCoverName:@"Franz Krieger" realName:@"Jean Reno" accessLevel:4 compromised:NO];
+        LSIAgent *agent6 = [[LSIAgent alloc] initWithCoverName:@"Luther Stickell" realName:@"Ving Rhames" accessLevel:4 compromised:NO];
+        LSIAgent *agent7 = [[LSIAgent alloc] initWithCoverName:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel:5 compromised:YES];
+        LSIAgent *agent8 = [[LSIAgent alloc] initWithCoverName:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel:4 compromised:NO];
+        LSIAgent *agent9 = [[LSIAgent alloc] initWithCoverName:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel:5 compromised:YES];
+        LSIAgent *agent10 = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:YES];
+        LSIAgent *agent11 = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:NO];
         
         // MARK: - Step 3
         
