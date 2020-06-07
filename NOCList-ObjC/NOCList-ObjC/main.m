c34cee8e5eb0f73f86d680925efdcf32c4c2b695
Shawn James <shawn.james@me.com>
2020-06-07 11:25:57 -0400

    Add starter project

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Shawn James on 6/7/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
