313f7f7a2808ee33fedfac97a072b7efadf4ec9d
Chris Dobek <dobek.christopher@gmail.com>
2020-06-08 16:21:46 -0400

    MVP

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -13,6 +13,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         int numberOfCompromisedAgents = 0;
+        int numberOfCleanAgents = 0;
+        int highLevelAgents = 0;
+        int mediumLevelAgents = 0;
+        int lowLevelAgents = 0;
       
         LSIAgent *ethan = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
         LSIAgent *jim = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
@@ -31,11 +35,30 @@ int main(int argc, const char * argv[]) {
         for(LSIAgent *agent in agents) {
             if([agent.compromised boolValue] == YES) {
                 numberOfCompromisedAgents += 1;
+            } else {
+                numberOfCleanAgents += 1;
+                NSLog(@"Agent is clean: %@", agent.coverName);
             }
         }
-
-        NSLog(@"There are %d compromised agents.", numberOfCompromisedAgents);
         
+        NSLog(@"There are %d compromised agents.", numberOfCompromisedAgents);
+        NSLog(@"There are %d clean agents left in the field.", numberOfCleanAgents);
+        
+        for (int i=0; i<agents.count; i++) {
+            if([[agents[i] accessLevel] intValue] >= 8) {
+                if([[agents[i] compromised] boolValue] == YES) {
+                    NSLog(@"%@, level: %@ **WARNING** **COMPROMISED**", [agents[i] realName], [agents[i] accessLevel]);
+                } else {
+                    NSLog(@"%@, level %@", [agents[i] realName], [agents[i] accessLevel]);
+                }
+                highLevelAgents += 1;
+            } else if([[agents[i] accessLevel] intValue] < 8 && [[agents[i] accessLevel] intValue] >= 5) {
+                mediumLevelAgents += 1;
+            } else {
+                lowLevelAgents += 1;
+            }
+        }
+        NSLog(@"%d low level agents, %d mid level agents, %d high level agents", lowLevelAgents, mediumLevelAgents, highLevelAgents);
     }
     return 0;
 }
