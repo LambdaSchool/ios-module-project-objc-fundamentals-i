432835d8a8075b3dcf850ebc98020d7201ff764e
Chris Dobek <dobek.christopher@gmail.com>
2020-06-08 16:15:30 -0400

    Step 3

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -11,6 +11,8 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        int numberOfCompromisedAgents = 0;
       
         LSIAgent *ethan = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
         LSIAgent *jim = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
@@ -25,6 +27,14 @@ int main(int argc, const char * argv[]) {
         LSIAgent *frank = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:NO];
 
         NSArray *agents = [[NSMutableArray alloc] initWithObjects:ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank, nil];
+        
+        for(LSIAgent *agent in agents) {
+            if([agent.compromised boolValue] == YES) {
+                numberOfCompromisedAgents += 1;
+            }
+        }
+
+        NSLog(@"There are %d compromised agents.", numberOfCompromisedAgents);
         
     }
     return 0;
