0a7b58c3d5e5aa4c8b9920921971db71933ed8af
Ezra Black <dev.ezrablack@gmail.com>
2020-06-08 16:24:31 -0500

    Base Project
    Added Base MacOS command line project.

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Ezra Black on 6/8/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
