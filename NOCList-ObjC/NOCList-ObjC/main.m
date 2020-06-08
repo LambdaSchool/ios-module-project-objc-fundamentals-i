ceb0af460215cb05ecfb24c05234f70109cafdf0
Chris Dobek <dobek.christopher@gmail.com>
2020-06-08 16:06:55 -0400

    Project Added

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Chris Dobek on 6/8/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
