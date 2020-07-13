c9c56d9b7e5d8abc65185f50f962b22ca36bc1d4
nonye <nnezekwo@gmail.com>
2020-07-13 15:59:56 -0700

    Step 2 & 3 complete

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -11,8 +11,31 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+     LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel: 8 compromised: YES];
+            LSIAgent *agent2 = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel: 8 compromised: YES];
+            LSIAgent *agent3 = [[LSIAgent alloc] initWithCoverName:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel: 8 compromised: YES];
+            LSIAgent *agent4 = [[LSIAgent alloc] initWithCoverName:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel: 8 compromised: YES];
+            LSIAgent *agent5 = [[LSIAgent alloc] initWithCoverName:@"Franz Krieger" realName:@"Jean Reno" accessLevel: 8 compromised: YES];
+            LSIAgent *agent6 = [[LSIAgent alloc] initWithCoverName:@"Luther Stickell" realName:@"Ving Rhames" accessLevel: 8 compromised: YES];
+            LSIAgent *agent7 = [[LSIAgent alloc] initWithCoverName:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel: 8 compromised: YES];
+            LSIAgent *agent8 = [[LSIAgent alloc] initWithCoverName:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel: 8 compromised: YES];
+            LSIAgent *agent9 = [[LSIAgent alloc] initWithCoverName:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel: 8 compromised: YES];
+            LSIAgent *agent10 = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel: 8 compromised: YES];
+            LSIAgent *agent11 = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel: 8 compromised: YES];
+
+            NSArray *agents =@[agent1,
+                               agent2,
+                               agent3,
+                               agent4,
+                               agent5,
+                               agent6,
+                               agent7,
+                               agent8,
+                               agent9,
+                               agent10,
+                               agent11];
+
+        
     }
     return 0;
 }
