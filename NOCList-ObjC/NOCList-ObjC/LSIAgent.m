6b9d07683dbc8e84464cadc6fe0b86202b28d7b3
Cody Morley <codymorleyinc@gmail.com>
2020-07-13 16:19:08 -0700

    MVP
    Finished.

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Bling Morley on 7/13/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Bling Morley on 7/13/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
