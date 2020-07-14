638972420ae4820cdad906ae92b0149d6c22a027
Harmony Radley <harmonyradley99@gmail.com>
2020-07-14 18:21:57 -0400

    This stuff is confusing. Almost done with step 1

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Harmony Radley on 7/14/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Harmony Radley on 7/14/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
