a55c3a58ddbd017ed959799f9c274dccad709e61
Matt Martindale <martindalematt18@yahoo.com>
2020-07-21 20:30:42 -0600

    Added code to call our methods

--- Objective-C Playground/Objective-C Playground/main.m
+++ Objective-C Playground/Objective-C Playground/main.m
@@ -7,11 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SyntaxBasics.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Declaring the class
+        SyntaxBasics *object = [[SyntaxBasics alloc] init];
+        // Calling the methods
+        [object instanceMethod];
+        [object instanceMethodWithString:@"Hello!"];
+        
     }
     return 0;
 }
