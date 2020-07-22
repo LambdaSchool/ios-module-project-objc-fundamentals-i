54ba54622a61ee8e8c6522740d24bee4f541e090
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-22 11:32:21 -0400

    Set up initial project

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Sean Acres on 7/22/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
