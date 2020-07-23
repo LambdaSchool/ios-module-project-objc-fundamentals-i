2c385df0ce3a10ee5849b7ad2cb92bea293eaa31
Chad Parker <chad@chadparker.com>
2020-07-22 19:51:34 -0700

    Create agents and put them in an array

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -7,11 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
+
+/*
+ coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false
+ coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: true
+ coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false
+ coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true
+ coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false
+ coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false
+ coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true
+ coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false
+ coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true
+ coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true
+ coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false
+ */
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        LSIAgent *ethan = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:false];
+        LSIAgent *jim = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:true];
+        LSIAgent *claire = [[LSIAgent alloc] initWithCoverName:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel:5 compromised:false];
+        LSIAgent *eugene = [[LSIAgent alloc] initWithCoverName:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel:10 compromised:true];
+        LSIAgent *franz = [[LSIAgent alloc] initWithCoverName:@"Franz Krieger" realName:@"Jean Reno" accessLevel:4 compromised:false];
+        LSIAgent *luther = [[LSIAgent alloc] initWithCoverName:@"Luther Stickell" realName:@"Ving Rhames" accessLevel:4 compromised:false];
+        LSIAgent *sarah = [[LSIAgent alloc] initWithCoverName:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel:5 compromised:true];
+        LSIAgent *max = [[LSIAgent alloc] initWithCoverName:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel:4 compromised:false];
+        LSIAgent *hannah = [[LSIAgent alloc] initWithCoverName:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel:5 compromised:true];
+        LSIAgent *jack = [[LSIAgent alloc] initWithCoverName:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:true];
+        LSIAgent *frank = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:false];
+
+        NSArray *agents = @[ethan, jim, claire, eugene, franz, luther, sarah, max, hannah, jack, frank];
+        NSLog(@"%lu agents", (unsigned long)agents.count);
+        //NSLog(@"%@", agents);
     }
     return 0;
 }
