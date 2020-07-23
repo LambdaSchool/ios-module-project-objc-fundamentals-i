e511e7192acf510da3b13886439867f26358111f
Jarren <jarrenmcampos@gmail.com>
2020-07-22 19:16:46 -0700

    Initial set up plus header implemented

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,14 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Jarren Campos on 7/22/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+
+@end
---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Jarren Campos on 7/22/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
