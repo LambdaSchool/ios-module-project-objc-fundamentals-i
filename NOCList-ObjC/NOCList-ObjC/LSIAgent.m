5093526da01d65f0a54d97b9f46af0faf160de90
Matt Martindale <martindalematt18@yahoo.com>
2020-07-22 20:01:31 -0600

    Defined init method in implementation file

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -10,4 +10,15 @@
 
 @implementation LSIAgent
 
+- (instancetype)initCoverName:(NSString *)coverName andRealName:(NSString *)realName andAccessLevel:(int)accessLevel andCompromised:(BOOL)compromised
+{
+    if (self = [super init]) {
+        self.coverName = coverName;
+        self.realName = realName;
+        self.accessLevel = [NSNumber numberWithInt:accessLevel];
+        self.compromised = [NSNumber numberWithBool:compromised];
+    }
+    return self;
+}
+
 @end
