5cc72c054c7cf222a962170695544eeb32ca6fb2
Gerardo <gahernandez02@gmail.com>
2020-07-22 20:45:42 -0700

    Created a for loop to determine the total amount of clean agents.

--- NOC List Objective-C/NOC List Objective-C/main.m
+++ NOC List Objective-C/NOC List Objective-C/main.m
@@ -11,7 +11,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-     
+        
         // MARK: - Step 2
         LSIAgent *agent1 = [[LSIAgent alloc] initWithCoverName:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
         LSIAgent *agent2 = [[LSIAgent alloc] initWithCoverName:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
@@ -26,6 +26,30 @@ int main(int argc, const char * argv[]) {
         LSIAgent *agent11 = [[LSIAgent alloc] initWithCoverName:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:NO];
         
         NSArray *agents = @[agent1, agent2, agent3, agent4, agent5, agent6, agent7, agent8, agent9, agent10, agent11];
+        
+        //MARK: - Step 3
+        int totalNumberOfCompromisedAgents = 0;
+        
+        for (LSIAgent *agent in agents) {
+            if ([agent.compromised isEqualToNumber:@YES]) {
+                totalNumberOfCompromisedAgents += 1;
+            }
+        }
+        
+        NSLog(@"The total number of compromised agents is %d.", totalNumberOfCompromisedAgents);
+        
+        //MARK: - Step 4
+        int totalNumberOfCleanAgents = 0;
+        
+        for (LSIAgent *agent in agents) {
+            if ([agent.compromised isEqualToNumber:@NO]) {
+                NSLog(@"Agent is clean: %@.", agent.coverName);
+                totalNumberOfCleanAgents += 1;
+            }
+        }
+        
+        NSLog(@"The total number of clean agents is %d.", totalNumberOfCleanAgents);
+        
     }
     return 0;
 }
