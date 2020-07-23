71c33d4a775514474d5d3d25170eccb65a67ffda
Claudia Maciel <clc_80@yahoo.com>
2020-07-22 17:59:56 -0700

    Created a new class

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,17 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Claudia Maciel on 7/22/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Claudia Maciel on 7/22/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
