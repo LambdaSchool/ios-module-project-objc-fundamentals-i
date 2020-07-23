c54d4ed90152d28e7e14f4dee66931b12a65915c
Claudia Maciel <clc_80@yahoo.com>
2020-07-22 17:59:19 -0700

    Added MacOS project

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Claudia Maciel on 7/22/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
