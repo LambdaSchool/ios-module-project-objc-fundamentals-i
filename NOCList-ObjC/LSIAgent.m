28e9183de640b3de80d700c286515020f08a0beb
kensam1 <samarakodykennedy@gmail.com>
2020-09-17 00:11:14 -0400

    NOCList

---
+++ NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,22 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Lambda_School_Loaner_241 on 9/16/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSIAgent: NSObject
+
++ (void)classMethod;
+
+@property (nonatomic) NSString *coverName;
+@property (nonatomic) NSString *realName;
+@property (nonatomic) NSNumber* accessLevel;
+@property (nonatomic) NSNumber* compromised;
+
+- (instancetype) initWithAgentProperties:
+
+// work on step 1. Seek help
---
+++ NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Lambda_School_Loaner_241 on 9/16/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
