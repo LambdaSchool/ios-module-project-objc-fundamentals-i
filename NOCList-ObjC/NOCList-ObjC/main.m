1fdc85d325d9bcb53838998a4f5df762702b617a
Libby Thomas <thomaslibby05@gmail.com>
2020-09-16 20:45:46 -0500

    Created agent objects and added them into an array

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -7,11 +7,93 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        LSIAgent *agentOne = [[LSIAgent alloc]
+                              initWithCoverName:@"Ethan Hunt"
+                              realName:@"Tom Cruise"
+                              accessLvel:8
+                              compromised:false];
+
+        LSIAgent *agentTwo = [[LSIAgent alloc]
+                              initWithCoverName:@"Jim Phelps"
+                              realName:@"Jon Voight"
+                              accessLvel:9
+                              compromised:true];
+
+        LSIAgent *agentThree = [[LSIAgent alloc]
+                                initWithCoverName:@"Claire Phelps"
+                                realName:@"Emmanuelle Beart"
+                                accessLvel:5
+                                compromised:false];
+
+        LSIAgent *agentFour = [[LSIAgent alloc]
+                               initWithCoverName:@"Eugene Kittridge"
+                               realName:@"Henry Czerny"
+                               accessLvel:10
+                               compromised:true];
+
+        LSIAgent *agentFive = [[LSIAgent alloc]
+                               initWithCoverName:@"Franz Krieger"
+                               realName:@"Jean Reno"
+                               accessLvel:4
+                               compromised:false];
+
+        LSIAgent *agentSix = [[LSIAgent alloc]
+                              initWithCoverName:@"Luther Stickell"
+                              realName:@"Ving Rhames"
+                              accessLvel:4
+                              compromised:false];
+
+        LSIAgent *agentSeven = [[LSIAgent alloc]
+                                initWithCoverName:@"Sarah Davies"
+                                realName:@"Kristin Scott Thomas"
+                                accessLvel:5
+                                compromised:true];
+
+        LSIAgent *agentEight = [[LSIAgent alloc]
+                                initWithCoverName:@"Max RotGab"
+                                realName:@"Vanessa Redgrave"
+                                accessLvel:4
+                                compromised:false];
+
+        LSIAgent *agentNine = [[LSIAgent alloc]
+                               initWithCoverName:@"Hannah Williams"
+                               realName:@"Ingeborga Dapunaite"
+                               accessLvel:5
+                               compromised:true];
+
+        LSIAgent *agentTen = [[LSIAgent alloc]
+                              initWithCoverName:@"Jack Harmon"
+                              realName:@"Emilio Estevez"
+                              accessLvel:6
+                              compromised:true];
+
+        LSIAgent *agentEleven = [[LSIAgent alloc]
+                                 initWithCoverName:@"Frank Barnes"
+                                 realName:@"Dale Dye"
+                                 accessLvel:9
+                                 compromised:false];
+
+        NSMutableArray *agents = [NSMutableArray new];
+        [agents addObject:agentOne];
+        [agents addObject:agentTwo];
+        [agents addObject:agentThree];
+        [agents addObject:agentFour];
+        [agents addObject:agentFive];
+        [agents addObject:agentSix];
+        [agents addObject:agentSeven];
+        [agents addObject:agentEight];
+        [agents addObject:agentNine];
+        [agents addObject:agentTen];
+        [agents addObject:agentEleven];
+
+
     }
     return 0;
 }
+
+
