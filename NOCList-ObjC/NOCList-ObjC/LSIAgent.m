069b9e25c45e80d2389431bc2d6789fe1dfcea1e
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-17 13:15:46 -0700

    Initalized Agents

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,12 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Clayton Watkins on 9/17/20.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -6,11 +6,23 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "LSIAgent.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Agent Initialization
+        LSIAgent *agent1 = [[LSIAgent alloc] init:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel: 8 compromised: NO];
+        LSIAgent *agent2 = [[LSIAgent alloc] init:@"Jim Phelps" realName:@"Jon Voight" accessLevel: 9 compromised: YES];
+        LSIAgent *agent3 = [[LSIAgent alloc] init:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel: 5 compromised: NO];
+        LSIAgent *agent4 = [[LSIAgent alloc] init:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel: 10 compromised: YES];
+        LSIAgent *agent5 = [[LSIAgent alloc] init:@"Franz Krieger" realName:@"Jean Reno" accessLevel: 4 compromised: NO];
+        LSIAgent *agent6 = [[LSIAgent alloc] init:@"Luther Stickell" realName:@"Ving Rhames" accessLevel: 4 compromised: NO];
+        LSIAgent *agent7 = [[LSIAgent alloc] init:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel: 5 compromised: YES];
+        LSIAgent *agent8 = [[LSIAgent alloc] init:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel: 4 compromised: NO];
+        LSIAgent *agent9 = [[LSIAgent alloc] init:@"Hannah Williams" realName:@"Ingeborga Dapkunaite" accessLevel: 5 compromised: YES];
+        LSIAgent *agent10 = [[LSIAgent alloc] init:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel: 6 compromised: YES];
+        LSIAgent *agent11 = [[LSIAgent alloc] init:@"Frank Barnes" realName:@"Dale Dye" accessLevel: 9 compromised: NO];
+       
     }
     return 0;
 }
