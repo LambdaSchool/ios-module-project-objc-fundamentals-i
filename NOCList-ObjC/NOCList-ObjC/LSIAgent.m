b5c2429ebbd880d32f97ff3455e570784aa89c85
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-17 13:50:36 -0700

    Created loop to check if an agent is compromised

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -9,4 +9,16 @@
 
 @implementation LSIAgent
 
+- (instancetype)init:(NSString *) coverName realName:(NSString *)realName accessLevel:(int)accessLevel compromised:(BOOL)compromised {
+    self = [super init];
+    if (self != nil) {
+        _coverName = coverName;
+        _realName = realName;
+        NSNumber *accessLevel_ = [NSNumber numberWithInt:accessLevel];
+        _accessLevel = accessLevel_;
+        NSNumber *compromised_ = [NSNumber numberWithBool:compromised];
+        _compromised = compromised_;
+    }
+    return self;
+}
 @end
--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -11,17 +11,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // Agent Initialization
-        LSIAgent *agent1 = [[LSIAgent alloc] init:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel: 8 compromised: NO];
-        LSIAgent *agent2 = [[LSIAgent alloc] init:@"Jim Phelps" realName:@"Jon Voight" accessLevel: 9 compromised: YES];
-        LSIAgent *agent3 = [[LSIAgent alloc] init:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel: 5 compromised: NO];
-        LSIAgent *agent4 = [[LSIAgent alloc] init:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel: 10 compromised: YES];
-        LSIAgent *agent5 = [[LSIAgent alloc] init:@"Franz Krieger" realName:@"Jean Reno" accessLevel: 4 compromised: NO];
-        LSIAgent *agent6 = [[LSIAgent alloc] init:@"Luther Stickell" realName:@"Ving Rhames" accessLevel: 4 compromised: NO];
-        LSIAgent *agent7 = [[LSIAgent alloc] init:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel: 5 compromised: YES];
-        LSIAgent *agent8 = [[LSIAgent alloc] init:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel: 4 compromised: NO];
-        LSIAgent *agent9 = [[LSIAgent alloc] init:@"Hannah Williams" realName:@"Ingeborga Dapkunaite" accessLevel: 5 compromised: YES];
-        LSIAgent *agent10 = [[LSIAgent alloc] init:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel: 6 compromised: YES];
-        LSIAgent *agent11 = [[LSIAgent alloc] init:@"Frank Barnes" realName:@"Dale Dye" accessLevel: 9 compromised: NO];
+        LSIAgent *agent1 = [[LSIAgent alloc] init:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
+        LSIAgent *agent2 = [[LSIAgent alloc] init:@"Jim Phelps" realName:@"John Voight" accessLevel:8 compromised:YES];
+        LSIAgent *agent3 = [[LSIAgent alloc] init:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel:5 compromised:NO];
+        LSIAgent *agent4 = [[LSIAgent alloc] init:@"Eugene kittridge" realName:@"Bill" accessLevel:10 compromised:YES];
+        LSIAgent *agent5 = [[LSIAgent alloc] init:@"Franz Krieger" realName:@"Jean Reno" accessLevel:4 compromised:NO];
+        LSIAgent *agent6 = [[LSIAgent alloc] init:@"Luther Stickell" realName:@"Ving Rhames" accessLevel:4 compromised:NO];
+        LSIAgent *agent7 = [[LSIAgent alloc] init:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel:5 compromised:YES];
+        LSIAgent *agent8 = [[LSIAgent alloc] init:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel:4 compromised:NO];
+        LSIAgent *agent9 = [[LSIAgent alloc] init:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel:5 compromised:YES];
+        LSIAgent *agent10 = [[LSIAgent alloc] init:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:YES];
+        LSIAgent *agent11 = [[LSIAgent alloc] init:@"Frank Barnes" realName:@"Dale Dyle" accessLevel:9 compromised:NO];;
         
         // Adding agents to an Array
         NSArray *agentArray;
@@ -36,9 +36,16 @@ int main(int argc, const char * argv[]) {
                            agent9,
                            agent10,
                            agent11
-                           ];
-       
+        ];
         
+        // Printing out a list of compromised agents
+        int i = 0;
+        for (id agent in agentArray) {
+            if ([agent compromised] == [NSNumber numberWithBool:YES]) {
+                i += 1;
+            }
+        }
+        NSLog(@"\n\n%i agents are compromised.", i);
         
         
     }
