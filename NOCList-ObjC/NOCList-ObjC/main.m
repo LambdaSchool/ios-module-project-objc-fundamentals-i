05d54b451df0f1fc4e87c3d14881affdfe7bd674
brys <brys1saclaus2@gmail.com>
2020-11-10 22:11:58 -0800

    Created NOCList-ObjC xcode project

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by BrysonSaclausa on 11/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
