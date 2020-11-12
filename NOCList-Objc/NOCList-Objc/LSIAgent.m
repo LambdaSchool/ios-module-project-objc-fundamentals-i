0f28ec03e8bb3762c9fbf1fd1f3e99655a31d0fe
Norlan Tibanear <ntibanear@gmail.com>
2020-11-12 15:01:07 -0500

    Done

--- NOCList-Objc/NOCList-Objc/LSIAgent.m
+++ NOCList-Objc/NOCList-Objc/LSIAgent.m
@@ -9,4 +9,18 @@
 
 @implementation LSIAgent
 
-@end
+- (instancetype)init:(NSString *) coverName realName:(NSString *)realName accessLevel:(int)accessLevel compromised:(BOOL)compromised {
+    self = [super init];
+    if (self != nil) {
+        _coverName = coverName;
+        _realName = realName;
+        NSNumber *accessLevel_ = [NSNumber numberWithInt:accessLevel];
+        _accessLevel = accessLevel_;
+        NSNumber *compromised_ = [NSNumber numberWithBool:compromised];
+        _compromised = compromised_;
+    }
+    return self;
+}
+    @end
+ 
+    
--- NOCList-Objc/NOCList-Objc/main.m
+++ NOCList-Objc/NOCList-Objc/main.m
@@ -6,10 +6,136 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        
+        // Agents Init
+        
+        LSIAgent *agentOne = [[LSIAgent alloc]
+                              initCoverName:@"Ethan Hunt"
+                              realName:@"Tom Cruise"
+                              accessLevel:8
+                              compromised:false];
+        
+        LSIAgent *agentTwo = [[LSIAgent alloc]
+                              initCoverName:@"Jim Phelps"
+                              realName:@"Jon Voight"
+                              accessLevel:9
+                              compromised:true];
+        
+        LSIAgent *agentThree = [[LSIAgent alloc]
+                              initCoverName:@"Claire Phelps"
+                              realName:@"Emmanuelle Beart"
+                              accessLevel:5
+                              compromised:false];
+        
+        LSIAgent *agentFour = [[LSIAgent alloc]
+                              initCoverName:@"Eugene Kittridge"
+                              realName:@"Henry Czerny"
+                              accessLevel:10
+                              compromised:true];
+        
+        LSIAgent *agentFive = [[LSIAgent alloc]
+                              initCoverName:@"Franz Krieger"
+                              realName:@"Jean Reno"
+                              accessLevel:4
+                              compromised:false];
+        
+        LSIAgent *agentSix = [[LSIAgent alloc]
+                              initCoverName:@"Luther Stickell"
+                              realName:@"Ving Rhames"
+                              accessLevel:4
+                              compromised:false];
+        
+        LSIAgent *agentSeven = [[LSIAgent alloc]
+                              initCoverName:@"Sarah Davies"
+                              realName:@"Kristin Scott Thomas"
+                              accessLevel:5
+                              compromised:true];
+        
+        LSIAgent *agentEight = [[LSIAgent alloc]
+                              initCoverName:@"Max RotGab"
+                              realName:@"Vanessa Redgrave"
+                              accessLevel:4
+                              compromised:false];
+        
+        LSIAgent *agentNine = [[LSIAgent alloc]
+                              initCoverName:@"Hannah Williams"
+                              realName:@"Ingeborga Dapunaite"
+                              accessLevel:5
+                              compromised:true];
+        
+        LSIAgent *agentTen = [[LSIAgent alloc]
+                              initCoverName:@"Jack Harmon"
+                              realName:@"Emilio Estevez"
+                              accessLevel:6
+                              compromised:true];
+        
+        LSIAgent *agentEleven = [[LSIAgent alloc]
+                              initCoverName:@"Frank Barnes"
+                              realName:@"Dale Dye"
+                              accessLevel:9
+                                 compromised:false];        
+        
+        // Agents Array
+        NSArray *agentArray;
+        agentArray = @[agentOne, agentTwo, agentThree, agentFour, agentFive, agentSix, agentSeven, agentEight, agentNine, agentTen, agentEleven];
+        
+        
+        // List of compromised agents
+        int compromisedAgents = 0;
+        
+        for (LSIAgent *agent in agentArray) {
+            if ([agent compromised] == [NSNumber numberWithBool:TRUE]) {
+                compromisedAgents += 1;
+            }
+        }
+        NSLog(@"Number of compromised agents: %d", compromisedAgents);
+        
+        
+        // List of clean agents
+        int cleanAgents = 0;
+        
+        for (LSIAgent *agent in agentArray) {
+            if ([agent compromised] == [NSNumber numberWithBool:FALSE]) {
+                cleanAgents += 1;
+                NSLog(@"Agent is clean: %@", [agent coverName]);
+            }
+        }
+        NSLog(@"Number of clean agents: %d", cleanAgents);
+        
+        
+        // List of high risk agents
+        for (LSIAgent *agent  in agentArray) {
+            if ([[agent accessLevel] isGreaterThanOrEqualTo:[NSNumber numberWithInt:8]]) {
+                if ([agent compromised] == [NSNumber numberWithBool:TRUE]) {
+                    NSLog(@"%@, level %@ clearance. **WARNING**COMPROMISED**", agent.realName, agent.accessLevel);
+                } else if ([agent compromised] == [NSNumber numberWithBool:FALSE]) {
+                    NSLog(@"%@, level %@ clearance", agent.realName, agent.accessLevel);
+                }
+            }
+        }
+        
+        
+        // List of agents access level
+        int lowLevelAgents = 0;
+        int middleLevelAgents = 0;
+        int highLevelAgents = 0;
+        
+        for (LSIAgent *agent in agentArray) {
+            if ([[agent accessLevel] isLessThanOrEqualTo:[NSNumber numberWithInt:4]]) {
+                lowLevelAgents += 1;
+            } else if (([[agent accessLevel] isLessThanOrEqualTo:[NSNumber numberWithInt:7]]) && ([[agent accessLevel] isGreaterThan:[NSNumber numberWithInt:4]])) {
+                middleLevelAgents += 1;
+            } else {
+                highLevelAgents += 1;
+            }
+        }
+        NSLog(@"%d low level agents, %d mid level agents, and %d high level agents", lowLevelAgents, middleLevelAgents, highLevelAgents);
+        
+        
         NSLog(@"Hello, World!");
     }
     return 0;
