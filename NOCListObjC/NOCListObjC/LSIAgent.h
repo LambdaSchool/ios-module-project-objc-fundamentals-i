c83f56f5de41b9d1f34da82376cc84e9d14bfd65
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-11-11 19:25:11 -0800

    Step 1 - Model with initializer

--- NOCListObjC/NOCListObjC/LSIAgent.h
+++ NOCListObjC/NOCListObjC/LSIAgent.h
@@ -11,6 +11,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIAgent : NSObject
 
+@property (nonatomic) NSString *coverName;
+@property (nonatomic) NSString *realName;
+@property (nonatomic) NSNumber *accessLevel;
+@property (nonatomic) NSNumber *compromised;
+
+- (instancetype)initWithCoverName:(NSString *)coverName
+                         realName:(NSString *)realName
+                      accessLevel:(int) accessLevel
+                      compromised:(BOOL) compromised;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- NOCListObjC/NOCListObjC/LSIAgent.m
+++ NOCListObjC/NOCListObjC/LSIAgent.m
@@ -9,4 +9,18 @@
 
 @implementation LSIAgent
 
+- (instancetype)initWithCoverName:(NSString *)coverName
+                         realName:(NSString *)realName
+                      accessLevel:(int)accessLevel
+                      compromised:(BOOL)compromised
+{
+    if (self = [super init]) {
+        self.coverName = coverName;
+        self.realName = realName;
+        self.accessLevel = [NSNumber numberWithInt:accessLevel];
+        self.compromised = [NSNumber numberWithBool:compromised];
+    }
+    return self;
+}
+
 @end
