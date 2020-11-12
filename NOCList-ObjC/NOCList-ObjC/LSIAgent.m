637c7f2c04c8318e785854c475a16c0c5d745776
brys <brys1saclaus2@gmail.com>
2020-11-11 21:08:59 -0800

    Step 1: created model class, implement custom init

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,18 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by BrysonSaclausa on 11/10/20.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+- (instancetype)init:(NSString *) codeName realName:(NSString *)realName accessLevel:(int)accessLevel compromised:(BOOL)compromised {
+    
+    
+
+    return self;
+}
+@end
--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -6,11 +6,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        LSIAgent *agent0 = [[LSIAgent alloc] init:@"" realName:@"" accessLevel:8 compromised:NO];
+       
+        
     }
     return 0;
 }
