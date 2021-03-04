556988148a8e970698a85ac3f6adc770a44f60f3
James <jameswilliammcdougall@gmail.com>
2021-03-03 21:10:10 -0600

    Step 2 Complete

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.h
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -25,4 +25,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)checkForCleanAgents;
 
+@end
+
 NS_ASSUME_NONNULL_END
--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -9,4 +9,29 @@
 
 @implementation LSIAgent
 
+//MARK: - Initializer -
+- (instancetype) initWithCoverName:(NSString *)coverName
+                 realName:(NSString *)realName
+                 accessLevel:(int)accessLevel
+                 compromised:(BOOL)compromised
+{
+    _coverName = coverName;
+    _realName = realName;
+    _accessLevel = [NSNumber numberWithInt:accessLevel];
+    _compromised = [NSNumber numberWithBool:compromised];
+    return self;
+}
+
+//MARK: - Methods
+- (void)checkForCleanAgents:(NSArray<LSIAgent*>*)agents {
+    int numberOfCleanAgents = 0;
+    for (LSIAgent *agent in agents) {
+        if (agent.compromised == [NSNumber numberWithBool:true]) {
+            numberOfCleanAgents += 1;
+            NSLog(@"Found a clean agent %@", agent.coverName);
+        }
+    }
+    NSLog(@"Number of clean agents %d", numberOfCleanAgents);
+}
+
 @end
